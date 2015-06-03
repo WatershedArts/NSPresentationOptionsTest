b8c9790c1fe353dd343331a1af446ff6d6123733
David Haylock <david.haylock@watershed.co.uk>
2015-06-03 09:23:39 +0100

    Initial Commit

---
+++ NSPresentationOptionsExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSPresentationOptionsExample
+//
+//  Created by David Haylock on 03/06/2015.
+//  Copyright (c) 2015 David Haylock. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NSPresentationOptionsExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSPresentationOptionsExample
+//
+//  Created by David Haylock on 03/06/2015.
+//  Copyright (c) 2015 David Haylock. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
